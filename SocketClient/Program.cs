658ecddc7928bfba9904928b641e666c0b03e63b
KRChungwol <ccbj0523@gmail.com>
2021-02-22 19:48:03 +0900

    210222 Socket Project

---
+++ SocketClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient {
+    internal class Program {
+        public static void Main(string[] args) {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+            // 소켓 객체 생성. 첫 번째 파라미터는 IP를 사용함, 두 번째 파라미터는 Stream 소켓을 사용한다는 것이고, 
+            // 세 번째 파라미터는 TCP 프로토콜을 사용한다는 것이다.
+
+            var ep = new IPEndPoint(IPAddress.Parse("192.168.219.106"), 7000);
+            // 소켓에 연결하기 위해 IPEndPoint 객체를 생성한다.
+            sock.Connect(ep);
+            // 소켓 연결
+
+            string cmd = string.Empty;
+            byte [] receiverBuff = new byte[8192];
+            
+            Console.WriteLine("Connected... Enter Q to Exit");
+
+            while ((cmd = Console.ReadLine()) != "Q") {
+                byte [] buff = Encoding.UTF8.GetBytes(cmd);
+
+                sock.Send(buff, SocketFlags.None);
+                // 소켓에 데이터를 보내기 위해 Send 메서드 사용
+
+                int n = sock.Receive(receiverBuff);
+                // 데이터를 수신하기 위해 Receive 메서드 사용
+
+                string data = Encoding.UTF8.GetString(receiverBuff, 0, n);
+
+                Console.WriteLine(data);
+            }
+
+            sock.Close();
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer {
+    internal class Program {
+        public static void Main(string[] args) {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // Socket 객체 생성
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 7000);
+            // 들어오는 모든 접속을 받아들이기 위해 ep 설정
+            
+            sock.Bind(ep);
+            // 어떤 포트를 사용할지 객체에 바인드함
+
+            sock.Listen(10);
+            // backlog는 동시에 여러 클라이언트가 접속했을 때 최대로 대기할 수 있는 클라이언트의 수를 뜻함.
+
+            Socket clientSock = sock.Accept();
+            // socket에 요청이 들어온다면 Accept
+
+            byte[] buff = new byte[8192];
+            // 데이터를 송수신하기 위한 버퍼
+
+            while ((!Console.KeyAvailable) & (Encoding.UTF8.GetString(buff, 0, clientSock.Receive(buff)) != "Q")) {
+                int n = clientSock.Receive(buff); 
+                // 서버에 들어오는 데이터를 받음
+
+                string data = Encoding.UTF8.GetString(buff, 0, n);
+                Console.WriteLine($"[Received] {DateTime.Now} | {data}");
+                // 받은 데이터를 인코딩해서 출력 
+                
+                clientSock.Send(buff, 0, n, SocketFlags.None);
+                // 받은 데이터를 다시 출력
+            }
+
+            clientSock.Close();
+            sock.Close();
+        }
+    }
+}
